cc1ca7ac237cb24635f0042f96c31b902ef87cef
Bavo Symens <bavo.symens@gmail.com>
2025-02-05 22:44:03 +0100

    fix data dir

--- src/weight.py
+++ src/weight.py
@@ -21,7 +21,7 @@ else:
     df = pd.DataFrame(columns=['Date', 'Age_Days', 'Weight_kg'])
 
 # Load WHO data
-data_file = os.path.join(data_dir, 'data/tab_wfa_girls_p_0_13.xlsx')
+data_file = os.path.join(data_dir, 'tab_wfa_girls_p_0_13.xlsx')
 who_data = pd.read_excel(data_file)
 
 # Process the data to get the percentiles you need
